217485826443545bb48b3f55662a5cf4d4a938cf
Leonard Reyes <primus.reyes@gmail.com>
2026-03-10 07:57:16 +0000

    route: home

---
+++ chirp/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chirper</title>
+</head>
+
+<body>
+    <x-layout>
+        <x-slot:title>
+            Welcome
+        </x-slot:title>
+        <div class="max-w-2xl mx-auto">
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <h1 class="text-3x1 font-bold">Welcome to Chirper !< /h1>
+                        <p class="mt-4 text-base-content/60">This is your brand new Laravel </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    
+    </x-layout>
+
+
+</body>
+
+</html>
